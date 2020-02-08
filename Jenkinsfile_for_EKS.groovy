9864b44a64a5807ac14fedcefb84c9288f75a036
ktalant <uchkunfan@gmail.com>
2020-02-08 14:31:22 -0500

    updated

--- Jenkinsfile_for_EKS.groovy
+++ Jenkinsfile_for_EKS.groovy
@@ -4,8 +4,8 @@ node {
         git 'https://github.com/ktalant/terraform-iaac-eks-burak.git'
     }
     stage("Provision EKS with terraform"){
-        sh 'wget https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip'
-        sh 'unzip terraform_0.12.20_linux_amd64.zip && ./terraform version'
+        // sh 'wget https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip'
+        // sh 'unzip terraform_0.12.20_linux_amd64.zip && ./terraform version'
 
     }
     stage("Set environment for Terraform"){
