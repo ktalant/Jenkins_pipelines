8a7caf50311a578816cb51f9e2dda6d972a3c739
ktalant <uchkunfan@gmail.com>
2020-02-08 11:42:49 -0500

    added properties

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,9 +1,10 @@
 node {
+    properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Choose the region you want to build the image to.', name: 'AMI_REGION')])])
+
     stage("Pull repo"){
         echo "Hello"
     }
     stage("Build image"){
-        properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Choose the region you want to build the image to.', name: 'AMI_REGION')])])
         sh "packer version"
     }
     stage("Send notification to Slack"){
