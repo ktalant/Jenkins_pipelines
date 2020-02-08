d751a87bb5312d08c9344f4708feb6594b8d056b
ktalant <uchkunfan@gmail.com>
2020-02-08 13:45:18 -0500

    updated

---
+++ Jenkinsfile_for_EKS.groovy
@@ -0,0 +1,14 @@
+node {
+    stage("Pull repo"){
+        git 'https://github.com/ktalant/terraform-iaac-eks-burak.git'
+    }
+    stage("Stage1"){
+        echo "Hello"
+    }
+    stage("Stage1"){
+        echo "Hello"
+    }
+    stage("Stage1"){
+        echo "Hello"
+    }
+}
