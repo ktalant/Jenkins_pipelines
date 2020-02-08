9fc6c18ee8236494770b30d3561203098bd50e03
ktalant <uchkunfan@gmail.com>
2020-02-08 12:51:07 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ node {
             [parameters([
                 choice(choices:['golden_ami', 'elk', 'tower', 'nagiosxi', 'gitlab', 'nexus', 'vault'], description: 'what tool would you like to build', name: 'TOOL_TO_PROVISION'), 
                 choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'choose the region', name: 'AMI_REGION')])])
-
+}
     stage("Pull repo"){
         git 'https://github.com/farrukh90/packer.git'
     }
