6361e3798445ad9a82f318604ef5b4ec224460f1
ktalant <uchkunfan@gmail.com>
2020-02-08 12:01:06 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,10 +2,11 @@ node {
     properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Choose the region you want to build the image to.', name: 'AMI_REGION')])])
 
     stage("Pull repo"){
-        echo "Hello"
+        git 'https://github.com/farrukh90/packer.git'
     }
     stage("Build image"){
         sh "packer version"
+        sh "packer build -var region=${AMI_REGION} tools/jenkins_example.json"
     }
     stage("Send notification to Slack"){
         slackSend channel: '#jenkins'
