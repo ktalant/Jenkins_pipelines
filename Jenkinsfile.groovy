922e4f56cac8077b8d32cd8d2c5d67d2f8cd2962
ktalant <uchkunfan@gmail.com>
2020-02-08 11:41:09 -0500

    multiple choice has been added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,9 +3,11 @@ node {
         echo "Hello"
     }
     stage("Build image"){
+        properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Choose the region you want to build the image to.', name: 'AMI_REGION')])])
         sh "packer version"
     }
     stage("Send notification to Slack"){
+        slackSend channel: '#jenkins'
         echo "Hello"
     }
     stage("Send email"){
