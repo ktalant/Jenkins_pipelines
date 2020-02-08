bad74ec9a1350f71c6e0778c2d2f231c46f7acc0
ktalant <uchkunfan@gmail.com>
2020-02-08 11:32:29 -0500

    Jenkinsfile added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,1 +1,14 @@
-
+node {
+    stage("Pull repo"){
+        echo "Hello"
+    }
+    stage("Build image"){
+        sh "packer version"
+    }
+    stage("Send notification to Slack"){
+        echo "Hello"
+    }
+    stage("Send email"){
+        echo "Hello"
+    }
+}
