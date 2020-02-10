7b96371cb7218454449b59cabbcef25b46409c67
ktalant <uchkunfan@gmail.com>
2020-02-10 16:04:46 -0500

    updated

--- Jenkinsfile_for_EKS.groovy
+++ Jenkinsfile_for_EKS.groovy
@@ -1,18 +1,25 @@
-node {
-    properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '3'))])
-    stage("Pull repo"){
-        git 'https://github.com/ktalant/terraform-iaac-eks-burak.git'
+node{
+    stage("Pull Repo"){
+        ws ("tmp/"){
+            git 'https://github.com/farrukh90/terraform-iaac-eks-burak.git'
+        }
+        
     }
-    stage("Provision EKS with terraform"){
-        // sh 'wget https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip'
-        // sh 'unzip terraform_0.12.20_linux_amd64.zip && ./terraform version'
-        sh 'pwd'
+    stage("Download Terraform"){
+        ws ("tmp/") {
+            sh "terraform version"
+            sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
+            sh "unzip -o terraform_0.12.19_linux_amd64.zip"
+            sh "./terraform version"}
     }
-    stage("Set environment for Terraform"){
-        sh 'ls -la'
-        sh "source setenv.sh -var-file=./configurations/dev/us-west-2/dev.tfvars"
+    stage("Set Backend"){
+        ws ("tmp/"){
+            sh "./terraform init"
+        }
     }
-    stage("Stage4"){
-        echo "Hello"
+    stage("Plan"){
+        ws ("tmp/") {
+            sh "./terraform plan -var-file configurations/dev/us-west-2/dev.tfvars"
+        }
     }
 }
