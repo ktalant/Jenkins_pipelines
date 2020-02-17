fd41ad555ef4a9a8d1c503ef749dc8ae81214488
ktalant <uchkunfan@gmail.com>
2020-02-16 20:59:54 -0500

    parameters has been added

--- Jenkinsfile_terraform.groovy
+++ Jenkinsfile_terraform.groovy
@@ -1,4 +1,11 @@
 pipeline {
+  properties([
+    buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '4')),
+    parameters([
+      string(defaultValue: '', description: 'Enter aws profile', name: 'AWS_PROFILE', trim: false),
+      string(defaultValue: '', description: 'Paste AWS access key id', name: 'AWS_ACCESS_KEY_ID', trim: false),
+      string(defaultValue: '', description: 'Paste AWS secret access key', name: 'AWS_SECRET_ACCESS_KEY', trim: false),
+      choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Choose the region', name: 'AWS_REGION')])])
 
   agent any
   stages {
