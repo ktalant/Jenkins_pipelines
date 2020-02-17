bd2923608e114001023df7e89641128303a8faf1
ktalant <uchkunfan@gmail.com>
2020-02-17 12:33:08 -0500

    parameters has been updated

--- Jenkinsfile_terraform1.groovy
+++ Jenkinsfile_terraform1.groovy
@@ -2,13 +2,19 @@ pipeline {
   agent any
 
   parameters {
-                string(name: 'AWS_REGION', description: 'Enter the region')
+                choice(name: 'AWS_REGION', choices: ['us-east-1', 'us-east-2', 'us-west-1'], description: ''),
+                string(name: 'AWS_ACCESS_KEY_ID', description: 'Paste here AWS access key id'),
+                string(name: 'AWS_SECRET_ACCESS_KEY', description: 'Paste here AWS secret access key id'),
+                string(name: 'AWS_PROFILE', description: 'Enter you profile')
             }
   stages {
 
     stage ('Configure AWS Credentials') {
       steps {
         echo 'Set up aws credentials to ${AWS_REGION}'
+        echo 'Here is your ${AWS_ACCESS_KEY_ID}'
+        echo 'Here is your ${AWS_SECRET_ACCESS_KEY}'
+        echo 'Here is your --${AWS_PROFILE}'
         sh '''
         ls -a
         '''
