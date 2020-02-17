9844e672f0ddfd574c2f561cb86a7ceb3aa4ec35
ktalant <uchkunfan@gmail.com>
2020-02-17 13:48:06 -0500

    updated

--- Jenkinsfile_terraform1.groovy
+++ Jenkinsfile_terraform1.groovy
@@ -11,12 +11,15 @@ pipeline {
 
     stage ('Configure AWS Credentials') {
       steps {
-        echo "Set up aws credentials to ${AWS_REGION}"
-        echo "Here is your ${AWS_ACCESS_KEY_ID}"
-        echo "Here is your ${AWS_SECRET_ACCESS_KEY}"
-        echo "Here is your --${AWS_PROFILE}"
+        // echo "Set up aws credentials to ${AWS_REGION}"
+        // echo "Here is your ${AWS_ACCESS_KEY_ID}"
+        // echo "Here is your ${AWS_SECRET_ACCESS_KEY}"
+        // echo "Here is your --${AWS_PROFILE}"
         sh '''
         ls -a
+          aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID} --profile ${AWS_PROFILE}
+          aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY} --profile ${AWS_PROFILE}
+          aws configure set region ${AWS_REGION} --profile ${AWS_PROFILE}
         '''
       }
     }
