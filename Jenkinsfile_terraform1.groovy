e9a6724bf4f7305a80ab475631880446652faa83
ktalant <uchkunfan@gmail.com>
2020-02-17 12:08:47 -0500

    added new parameter

--- Jenkinsfile_terraform1.groovy
+++ Jenkinsfile_terraform1.groovy
@@ -2,8 +2,12 @@ pipeline {
   agent any
   stages {
     stage ('Configure AWS Credentials') {
+      parameters {
+                    string(name: 'AWS_REGION', description: 'Enter the region')
+                }
+            }
       steps {
-        echo 'Set up aws credentials'
+        echo 'Set up aws credentials to ${AWS_REGION}'
         sh '''
         ls -a
         '''
