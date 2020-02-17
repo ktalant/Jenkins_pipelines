16d94f401065876efa4421f66eb0039f8ecf7ef7
ktalant <uchkunfan@gmail.com>
2020-02-17 13:15:18 -0500

    Discard old builds has been added

--- Jenkinsfile_terraform1.groovy
+++ Jenkinsfile_terraform1.groovy
@@ -1,6 +1,6 @@
 pipeline {
   agent any
-
+  options { buildDiscarder(logRotator(numToKeepStr: '4')) }
   parameters {
                 choice(name: 'AWS_REGION', choices: ['us-east-1', 'us-east-2', 'us-west-1'], description: '')
                 string(name: 'AWS_ACCESS_KEY_ID', description: 'Paste here AWS access key id')
